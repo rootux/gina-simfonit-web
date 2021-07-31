5499e0a3ee8975e20449071c21f7b985637070ce
Gal Bracha <galbra@gmail.com>
2021-07-31 23:13:10 +0300

    - Added a new music player - tune + refactored

--- docs/js/garden_player.js
+++ docs/js/garden_player.js
@@ -2,92 +2,29 @@ let inited = false;
 let delay;
 let chorus;
 let source;
-let audioContext;
 
 window.player = {}
 
 function init() {
-    //create an audio context and load an example audio file
-    initAudioContext().then(function() {
-        //create an instance of Tuna by passing the AudioContext we use
-        const tuna = new Tuna(audioContext);
-        //create a new Tuna delay instance
-        delay = new tuna.Delay({
-            delayTime: 10 //a short delayTime to create a slap-back delay
-        });
+    makeChannel(-)
+}
 
-        chorus = new tuna.Chorus({
-            rate: 5.5,
-            feedback: 0.2,
-            delay: 0.0045,
-            bypass: 0
-        });
-        //connect the source to the Tuna delay
-        source.connect(delay);
-        //connect delay as a standard web audio node to the audio context destination
-        delay.connect(audioContext.destination);
+function makeChannel(name, url, pan=0) {
+    const channel = new Tone.Channel({
+        pan
+    }).toDestination();
+    const player = new Tone.Player({
+        url: `https://tonejs.github.io/audio/berklee/${url}.mp3`,
+        loop: true
+    }).sync().start(0);
+    player.connect(channel);
 
-        source.connect(chorus);
-        chorus.connect(audioContext.destination);
-        //start playing!
-        source.start(audioContext.currentTime);
-
-        inited = true;
+    // add a UI element
+    ui({
+        name,
+        tone: channel,
+        parent: document.querySelector("#content")
     });
-}
-
-/*
-    This is just the boilerplate needed to load an audio file, create the audio context and provide the dry/wet button functionality
-*/
-
-function initAudioContext() {
-    return new Promise(function(resolve, reject) {
-        const AC = "AudioContext" in window ? AudioContext : "webkitAudioContext" in window ? webkitAudioContext : document.write("Web Audio not supported");
-        audioContext = new AC();
-        source = audioContext.createBufferSource();
-        const format = checkAudioFormat();
-        const xhr = new XMLHttpRequest();
-
-        xhr.open("GET", "/sounds/london." + format);
-        xhr.responseType = "arraybuffer";
-        xhr.onload = function(e) {
-            audioContext.decodeAudioData(e.target.response, function(b) {
-                source.buffer = b;
-                source.loop = true;
-                resolve();
-            });
-        }
-        xhr.send(null);
-
-        xhr.open("GET", "/sounds/seoul." + format);
-        xhr.responseType = "arraybuffer";
-        xhr.onload = function(e) {
-            audioContext.decodeAudioData(e.target.response, function(b) {
-                source.buffer = b;
-                source.loop = true;
-                resolve();
-            });
-        }
-        xhr.send(null);
-    })
-
-}
-
-/*
-    Check file format to use
-*/
-
-function checkAudioFormat () {
-    const elem = document.createElement('audio');
-    if (elem.canPlayType) {
-        // if (elem.canPlayType('audio/ogg; codecs="vorbis"').replace(/^no$/, '')) {
-        //     return "ogg";
-        // }
-        if (elem.canPlayType('audio/mpeg; codecs="mp3"').replace(/^no$/, '')) {
-            return "mp3";
-        }
-    }
-    return false;
 }
 
 window.player.init = function() {
--- static/js/garden_player.js
+++ static/js/garden_player.js
@@ -1,131 +1,95 @@
 let inited = false;
-let delay;
-let chorus;
-let source;
-let audioContext;
-
+let channels = []
+let buttons = {}
 window.player = {}
 
-function init() {
-    //create an audio context and load an example audio file
-    initAudioContext().then(function() {
-        //create an instance of Tuna by passing the AudioContext we use
-        const tuna = new Tuna(audioContext);
-        //create a new Tuna delay instance
-        delay = new tuna.Delay({
-            delayTime: 10 //a short delayTime to create a slap-back delay
-        });
+let soundA = "/sounds/seoul.mp3"
+let soundB = "/sounds/london.mp3"
+let soundC = "/sounds/tokyo.mp3"
 
-        chorus = new tuna.Chorus({
-            rate: 5.5,
-            feedback: 0.2,
-            delay: 0.0045,
-            bypass: 0
-        });
-        //connect the source to the Tuna delay
-        source.connect(delay);
-        //connect delay as a standard web audio node to the audio context destination
-        delay.connect(audioContext.destination);
-
-        source.connect(chorus);
-        chorus.connect(audioContext.destination);
-        //start playing!
-        source.start(audioContext.currentTime);
-
-        inited = true;
-    });
+async function init() {
+    channels.push(makeChannel(soundA, false));
+    channels.push(makeChannel(soundB, false));
+    channels.push(makeChannel(soundC, false));
+    setAllButtonsToActive();
+    inited = true;
 }
 
-/*
-    This is just the boilerplate needed to load an audio file, create the audio context and provide the dry/wet button functionality
-*/
+function setAllButtonsToActive(){
+    buttons.windDir.classList.add('active');
+    buttons.windSpeed.classList.add('active');
+    buttons.waterStatus.classList.add('active');
+}
 
-function initAudioContext() {
-    return new Promise(function(resolve, reject) {
-        const AC = "AudioContext" in window ? AudioContext : "webkitAudioContext" in window ? webkitAudioContext : document.write("Web Audio not supported");
-        audioContext = new AC();
-        source = audioContext.createBufferSource();
-        const format = checkAudioFormat();
-        const xhr = new XMLHttpRequest();
-
-        xhr.open("GET", "/sounds/london." + format);
-        xhr.responseType = "arraybuffer";
-        xhr.onload = function(e) {
-            audioContext.decodeAudioData(e.target.response, function(b) {
-                source.buffer = b;
-                source.loop = true;
-                resolve();
-            });
-        }
-        xhr.send(null);
-
-        xhr.open("GET", "/sounds/seoul." + format);
-        xhr.responseType = "arraybuffer";
-        xhr.onload = function(e) {
-            audioContext.decodeAudioData(e.target.response, function(b) {
-                source.buffer = b;
-                source.loop = true;
-                resolve();
-            });
-        }
-        xhr.send(null);
-    })
+function setAllButtonsToInactive() {
 
 }
 
-/*
-    Check file format to use
-*/
+function makeChannel(url, mute, pan=0) {
+    const channel = new Tone.Channel({
+        pan,
+        mute
+    }).toDestination();
+    const player = new Tone.Player({
+        url,
+        loop: true,
+        autostart:true
+    }).sync();
+    player.connect(channel);
+    return channel;
+}
 
-function checkAudioFormat () {
-    const elem = document.createElement('audio');
-    if (elem.canPlayType) {
-        // if (elem.canPlayType('audio/ogg; codecs="vorbis"').replace(/^no$/, '')) {
-        //     return "ogg";
-        // }
-        if (elem.canPlayType('audio/mpeg; codecs="mp3"').replace(/^no$/, '')) {
-            return "mp3";
-        }
-    }
-    return false;
+function getById(id) {
+    return document.getElementById(id);
+}
+
+function setBtns(state) {
+    const windDir = getById("wind_dir");
+    const windSpeed = getById("wind_speed");
+    const quiet = getById("quiet");
+    const waterStatus = getById("water_status");
+    windDir.disabled = state;
+    quiet.disabled = state;
+    windSpeed.disabled = state;
+    waterStatus.disabled = state;
 }
 
 window.player.init = function() {
     console.log("Loaded")
-    const start = document.getElementById("all");
-    const dry = document.getElementById("wind");
-    const wet = document.getElementById("wind_speed");
-    const quiet = document.getElementById("quiet");
-    dry.disabled = true;
-    wet.disabled = true;
-    quiet.disabled = true;
+    buttons.start = getById("all");
+    buttons.windDir = getById("wind_dir");
+    buttons.windSpeed = getById("wind_speed");
+    buttons.waterStatus = getById("water_status");
+    buttons.quiet = getById("quiet");
+    setBtns(true);
 
-    start.addEventListener("click", function(e) {
-        if (!inited) init();
-        dry.disabled = false;
-        quiet.disabled = false;
-        wet.disabled = false;
-        start.disabled = true;
+    buttons.start.addEventListener("click", async function(e) {
+        await Tone.start()
+        if (!inited) await init();
+        Tone.Transport.start()
+        setBtns(false);
+        buttons.start.disabled = true;
     });
 
-    quiet.addEventListener("click", function(e) {
-        if (!inited) init();
-        source.stop()
-        quiet.disabled = true;
-        start.disabled = false;
-        dry.disabled = true;
-        wet.disabled = true;
-        quiet.disabled = true;
-        inited = false;
+    buttons.quiet.addEventListener("click", function(e) {
+        Tone.Transport.stop();
+        buttons.start.disabled = false;
+        setBtns(true);
     });
 
-    dry.addEventListener("click", function(e) {
-        if (!inited) init();
-        if (delay) delay.bypass = !delay.bypass;
-    });
+    setClickHandler(buttons.windDir, 0);
+    setClickHandler(buttons.windSpeed, 1);
+    setClickHandler(buttons.waterStatus, 2);
+}
 
-    wet.addEventListener("click", function(e) {
-        if (!inited) init();
-        if (chorus) chorus.bypass = false;
+function setClickHandler(btn, channelIndex) {
+    btn.addEventListener("click", function(e) {
+        const isActive = btn.classList.contains('active');
+        if(!isActive) {
+            btn.classList.add('active')
+        } else {
+            btn.classList.remove('active')
+        }
+        channels[channelIndex].set({mute: isActive});
     });
 }
