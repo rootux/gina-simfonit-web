2ebc3dd13a50647b27c12d12325872c4a03d8b0a
Gal Bracha <galbra@gmail.com>
2021-08-12 14:47:36 +0300

    Added music. Fixed garden player

--- docs/js/garden_player.js
+++ docs/js/garden_player.js
@@ -3,26 +3,48 @@ let channels = []
 let buttons = {}
 window.player = {}
 
-let soundA = "/sounds/seoul.mp3"
-let soundB = "/sounds/london.mp3"
-let soundC = "/sounds/tokyo.mp3"
+let soundA = "/sounds/T1.mp3"
+let soundB = "/sounds/T2.mp3"
+let soundC = "/sounds/T3.mp3"
+let soundD = "/sounds/TBass.mp3"
+let soundE = "/sounds/v10.mp3"
+
+const buttonNames = ["start", "quiet",
+    "windSpeed", "windDir", "earth",
+    "radiation", "dust", "temperature", "humidity",
+    ];
 
 async function init() {
+    document.getElementById("loadingSounds").style.display = ""
+    const weather = await getWeatherData();
+    console.log(weather);
     channels.push(makeChannel(soundA, false));
     channels.push(makeChannel(soundB, false));
     channels.push(makeChannel(soundC, false));
+    channels.push(makeChannel(soundD, false));
+    channels.push(makeChannel(soundE, false));
     setAllButtonsToActive();
     inited = true;
+    Tone.loaded().then(() => {
+        document.getElementById("loadingSounds").style.display = "none"
+        console.log("Loaded all sounds")
+    })
 }
 
 function setAllButtonsToActive(){
-    buttons.windDir.classList.add('active');
-    buttons.windSpeed.classList.add('active');
-    buttons.waterStatus.classList.add('active');
+    Object.keys(buttons).forEach((button) => {
+        buttons[button].disabled = false;
+        buttons[button].classList.add('active');
+    })
 }
 
-function setAllButtonsToInactive() {
-
+function setAllButtonsToInactiveBut(btnToKeepActive) {
+    Object.keys(buttons).forEach((button) => {
+        if(button !== btnToKeepActive) {
+            buttons[button].disabled = true;
+            buttons[button].classList.remove('active');
+        }
+    })
 }
 
 function makeChannel(url, mute, pan=0) {
@@ -43,43 +65,38 @@ function getById(id) {
     return document.getElementById(id);
 }
 
-function setBtns(state) {
-    const windDir = getById("wind_dir");
-    const windSpeed = getById("wind_speed");
-    const quiet = getById("quiet");
-    const waterStatus = getById("water_status");
-    windDir.disabled = state;
-    quiet.disabled = state;
-    windSpeed.disabled = state;
-    waterStatus.disabled = state;
-}
-
 window.player.init = function() {
-    console.log("Loaded")
-    buttons.start = getById("all");
-    buttons.windDir = getById("wind_dir");
-    buttons.windSpeed = getById("wind_speed");
-    buttons.waterStatus = getById("water_status");
-    buttons.quiet = getById("quiet");
-    setBtns(true);
+    console.log("Loading sounds")
+    init()
+    Object.values(buttonNames).forEach((button) => {
+        buttons[button] = getById(button);
+    })
+    setAllButtonsToInactiveBut("start")
 
     buttons.start.addEventListener("click", async function(e) {
         await Tone.start()
         if (!inited) await init();
         Tone.Transport.start()
-        setBtns(false);
+        setAllButtonsToActive()
         buttons.start.disabled = true;
     });
 
     buttons.quiet.addEventListener("click", function(e) {
-        Tone.Transport.stop();
-        buttons.start.disabled = false;
-        setBtns(true);
+        if(buttons.start.disabled) {
+            Tone.Transport.start()
+            setAllButtonsToActive()
+        }else {
+            Tone.Transport.pause();
+            setAllButtonsToInactiveBut("quiet")
+        }
     });
 
     setClickHandler(buttons.windDir, 0);
     setClickHandler(buttons.windSpeed, 1);
-    setClickHandler(buttons.waterStatus, 2);
+    setClickHandler(buttons.earth, 2);
+    setClickHandler(buttons.radiation, 3);
+    setClickHandler(buttons.dust, 4);
+
 }
 
 function setClickHandler(btn, channelIndex) {
@@ -90,6 +107,6 @@ function setClickHandler(btn, channelIndex) {
         } else {
             btn.classList.remove('active')
         }
-        channels[channelIndex].set({mute: isActive});
+        channels[channelIndex].set({solo: isActive});
     });
 }
